1937ca7027705e72effeadd29f0436641ea003b5
Morag S <morags@users.noreply.github.com>
2018-03-03 00:41:25 +0200

    Incorporated most of $data into the template

--- 2018Template.php
+++ 2018Template.php
@@ -9,6 +9,17 @@ class Skin2018Template extends BaseTemplate {
 	 * Outputs the entire contents of the page
 	 */
 	public function execute() {
+
+		// Rearrange parts of $this->data for easier handling in the template
+		unset($this->data['nav_urls']['mainpage']);
+		if ( isset( $this->data['content_navigation']['actions']['watch'] ) ) {
+			$this->data['watch_indicator']['watch'] = $this->data['content_navigation']['actions']['watch'];
+			unset( $this->data['content_navigation']['actions']['watch'] );
+		} else {
+			$this->data['watch_indicator']['unwatch'] = $this->data['content_navigation']['actions']['unwatch'];
+			unset( $this->data['content_navigation']['actions']['unwatch'] );
+		}
+
 		echo $this->get( 'headelement' );	// Loads <head>
 		
 		$templateParser = new TemplateParser(  __DIR__ . '/resources/templates' );
