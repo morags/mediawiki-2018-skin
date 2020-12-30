2efb54ee4e221296854dc6a1d28328ebe339c582
jdlrobson <jdlrobson@gmail.com>
2020-12-30 09:33:53 -0800

    MediaWiki 1.35 support
    * Our version of Mustache does not support the `for`  watch indicator
    * The use of setupSkinUserCss is deprecated. Will break in future versions.
    Use skin registration instead (much cleaner and available since 1.35)
    * Prefix skin name with 's'. Skin keys that are numbers are not currently supported
    * Update to manifest version 2

--- includes/Skin2018.php
+++ includes/Skin2018.php
@@ -6,33 +6,5 @@
  */
 class Skin2018 extends SkinTemplate {
 	public $skinname = '2018';
-	public $stylename = '2018';
 	public $template = 'Skin2018Template';
-
-	/**
-	 * Add CSS via ResourceLoader
-	 *
-	 * @param $out OutputPage
-	 */
-	public function initPage( OutputPage $out ) {
-		$out->addMeta( 'viewport', 'width=device-width, initial-scale=1.0' );
-
-		$out->addModuleStyles( [
-			// 'mediawiki.skinning.elements',
-			// 'mediawiki.skinning.content',
-			// 'mediawiki.skinning.interface',
-			'mediawiki.skinning.content.externallinks',
-			'skins.2018'
-		 ] );
-		$out->addModules( [
-			'skins.2018.js'
-		 ] );
-	}
-
-	/**
-	 * @param $out OutputPage
-	 */
-	function setupSkinUserCss( OutputPage $out ) {
-		parent::setupSkinUserCss( $out );
-	}
 }
